0a9c5951492e7063a293bb369c1e9653ebf11fc7
arthiatsr <arthiatsr@users.noreply.github.com>
2019-12-31 16:35:49 -0600

    deploying to heroku

---
+++ client/build/precache-manifest.a5fc333fcc336b46700530de73b4c61a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a33324321055285cf24fa1bc645a65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14badf13aa64575ca8ee",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "130f2f3c849fe032cdc8",
+    "url": "/static/css/main.ad271978.chunk.css"
+  },
+  {
+    "revision": "14badf13aa64575ca8ee",
+    "url": "/static/js/2.8b50d140.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/static/js/2.8b50d140.chunk.js.LICENSE"
+  },
+  {
+    "revision": "130f2f3c849fe032cdc8",
+    "url": "/static/js/main.a0f1c7f4.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
